8c60f3db0f44351cb749f8e3edacf534d86d489b
Codery <67889100+CoderMoray@users.noreply.github.com>
2021-03-30 16:51:25 +0800

    Create AMGbase.py
    add some basic structure

---
+++ AMG_base/AMGbase.py
@@ -0,0 +1,20 @@
+import os, sys
+import pandas as pd
+import numpy as np
+from numba import njit, prange, cuda
+
+def AMGiterator(A,x,b,e):
+    """
+    """
+
+def P_creator(A):
+    """
+    """
+    n = A.shape[0]
+    
+
+
+def Gauss():
+    """
+    Gauss-Seidel interpolation
+    """
